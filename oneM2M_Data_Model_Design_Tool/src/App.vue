4282440d0ab16a0cc809ab9400c9c62127071488
JooSung, Kim <39945981+LAMXkE@users.noreply.github.com>
2023-10-30 22:00:43 +0900

    dev : child Resource validation add child resource validation routine

--- oneM2M_Data_Model_Design_Tool/src/App.vue
+++ oneM2M_Data_Model_Design_Tool/src/App.vue
@@ -3,32 +3,17 @@
     <div class="col-8">
           <div style="height: 500px; width: 100%; border: 1px solid red; position: relative; padding: 25px;">
             
-            <!-- <vue-draggable-resizable
-            :w="300"
-            :h="auto"
-            :resizable="false"
-            parent="true"
-            dragHandle=".drag-handle"
-            style="background-color:azure;"
-            :x="100"
-            :y="100"
-            >
-            <div class="drag-handle serverName">
-              <p>CSE 1</p>  
-            </div>
-            <div>
-              
-            
-            </div>
-          </vue-draggable-resizable> -->
           <nestedDraggable 
             :tasks="cse1"
             style="padding-left: 0px;"
-            :group="{name : 'cse', pull:'clone', put: true }"
-            @change="log"
+            :group="{
+                      name: 'resourceTree', 
+                      pull: true,
+                      put: true
+                    }"
             :min-height="200"
             item-key="id"
-            @end="moveTasks"
+            :dragoverBubble="true"
              >
 
              </nestedDraggable>
@@ -40,11 +25,8 @@
         class="dragArea resources list-items"
         :tasks="resources"
         style="padding-left: 0px;"
-        :group="{name : 'cse', pull: 'none', put: false }"
+        :group="{name:'resources', pull: 'clone', put: false}"
         item-key="id"
-        :clone="cloneResource"
-        :move="validateMove"
-        @end="moveTasks"
       >
       </nestedDraggable>
       <div class="buttonBox">
@@ -68,11 +50,6 @@ import draggable from "vuedraggable";
 import VueDraggableResizable  from "vue-draggable-resizable-vue3";
 import nestedDraggable from "@/components/infra/nested.vue";
 
-
-// import rawDisplayer from "./infra/raw-displayer.vue";
-
-// import ResourceCanvas from "./components/ResourceCanvas.vue";
-let idGlobal = 15;
 const RT_CSE = 5;
 const RT_AE = 2;
 const RT_CNT = 3;
@@ -85,8 +62,6 @@ const RT_TSI = 9;
 const RT_TSR = 10;
 const RT_MGMTOBJ = 11;
 const RT_NODE = 14;
-let target = undefined;
-let dragged = undefined;
 
 export default {
   name: "App",
@@ -104,77 +79,29 @@ export default {
       {
           name: "CSE1",
           ty: RT_CSE,
+          parent: undefined,
           tasks: [
           ]
         }
       ],
       resources: [
-        { name: "CSE", id: 1, ty: RT_CSE, tasks: [
-        { name: "AE", id: 2, ty: RT_AE, tasks:[
-        ]  },
-        { name: "CNT", id: 3, ty: RT_CNT },
-        { name: "ACP", id: 4, ty: RT_ACP },
-        { name: "GRP", id: 5, ty: RT_GRP },
-        { name: "SUB", id: 6, ty: RT_SUB },
-        { name: "FCNT", id: 7, ty: RT_FCNT },
+        { name: "CSE", id: 1  , ty: RT_CSE, tasks: [
+          { name: "AE", id: 2, ty: RT_AE, tasks:[
+          ]  },
+          { name: "CNT", id: 3, ty: RT_CNT },
+          { name: "ACP", id: 4, ty: RT_ACP },
+          { name: "GRP", id: 5, ty: RT_GRP },
+          { name: "SUB", id: 6, ty: RT_SUB },
+          { name: "FCNT", id: 7, ty: RT_FCNT },
         ] },
+        
         
       ]
     };
   },
   methods: {
     log: function(evt) {
-      window.console.log(evt);
-    },
-    cloneResource(evt) {
-      
-      console.log("cloneResource", evt);
-      return {
-        id: idGlobal++,
-        name: evt.name,
-        ty: evt.ty,
-        tasks: []
-      };
-    },
-    moveTasks(){
-    },
-    validateMove(evt) {
-      if(!this.isChildAvailable(evt.relatedContext.element.ty, evt.draggedContext.element.ty)){
-        console.log("Child not available");
-        return false;
-      }
-      if (evt.from == evt.to) return false;
-      target = evt.relatedContext.element;
-      dragged = evt.draggedContext.element;
-      return true;
-    },
-    isChildAvailable(parent_ty, child_ty){
-      console.log(parent_ty, child_ty);
-      if(parent_ty == undefined) return false;
-      switch(parent_ty){
-        case RT_CSE:
-          if(child_ty == RT_AE || child_ty == RT_ACP || child_ty == RT_GRP || child_ty == RT_SUB || child_ty == RT_FCNT || child_ty == RT_TS || child_ty == RT_TSI || child_ty == RT_TSR || child_ty == RT_MGMTOBJ || child_ty == RT_NODE){
-            return true;
-          }
-          break;
-        case RT_AE:
-          if(child_ty == RT_CNT || child_ty == RT_ACP || child_ty == RT_GRP || child_ty == RT_SUB || child_ty == RT_FCNT || child_ty == RT_TS || child_ty == RT_TSI || child_ty == RT_TSR || child_ty == RT_MGMTOBJ || child_ty == RT_NODE){
-            return true;
-          }
-          break;
-        case RT_CNT:
-          if(child_ty == RT_ACP || child_ty == RT_GRP || child_ty == RT_SUB || child_ty == RT_FCNT || child_ty == RT_TS || child_ty == RT_TSI || child_ty == RT_TSR || child_ty == RT_MGMTOBJ || child_ty == RT_NODE){
-            return true;
-          }
-          break;
-        case RT_ACP:
-          return false;
-        case RT_GRP:
-          return false;
-        case RT_SUB:
-          return false;
-      }
-      
+      // window.console.log(evt);
     },
     saveResourceTree(){
       console.log("saveResourceTree");
@@ -182,7 +109,7 @@ export default {
     },
     createResourceTree(){
       console.log("createResourceTree");
-    }
+    },
   }
 };
 </script>
@@ -190,7 +117,7 @@ export default {
 
 .dragArea {
   border: 1px solid red;
-  /* position: relative; */
+  position: relative;
 
 }
 
--- oneM2M_Data_Model_Design_Tool/src/components/infra/nested.vue
+++ oneM2M_Data_Model_Design_Tool/src/components/infra/nested.vue
@@ -2,23 +2,54 @@
     <draggable
       class="dragArea"
       tag="ul"
-      :list="tasks"
       :clone="cloneResource"
-      :group="{ name: 'cse', pull: 'clone', put: true }"
+      :list="tasks"
+      :group="this.group"
+      :sort="true"
       @change="log"
-      item-key="name"
+      item-key="id"
+      :move="validateMove"
+      @end="addParentResource"
     >
       <template #item="{ element }">
         <li class="resourceBox">
           <p>{{ element.name }}</p>
-          <nested-draggable :tasks="element.tasks" />
+          <nested-draggable 
+            v-if="element.tasks" 
+            :tasks="element.tasks" 
+            :group="this.group"
+            :childRT="getChildRT(element.ty)"
+            />
         </li>
       </template>
     </draggable>
   </template>
+
+
   <script>
   import draggable from "vuedraggable";
+  import {v4 as uuidv4} from 'uuid';
+  const RT_CSE = 5;
+  const RT_AE = 2;
+  const RT_CNT = 3;
+  const RT_ACP = 4;
+  const RT_GRP = 9;
+  const RT_SUB = 16;
+  const RT_FCNT = 7;
+  const RT_TS = 8;
+  const RT_TSI = 9;
+  const RT_TSR = 10;
+  const RT_MGMTOBJ = 11;
+  const RT_NODE = 14;
 
+  const resourceStructure = {
+    5:[RT_AE, RT_GRP, RT_MGMTOBJ, RT_ACP, RT_FCNT, RT_CNT, RT_SUB],
+    2:[RT_CNT, RT_GRP, RT_SUB, RT_FCNT, RT_TS, RT_TSI, RT_TSR, RT_MGMTOBJ, RT_NODE],
+    3:[RT_FCNT, RT_TS],
+    4:[],
+    9:[],
+    16:[],
+  }
 
   export default {
     props: {
@@ -26,28 +57,80 @@
         required: true,
         type: Array
       },
+      group: {
+        required: true,
+        type: Object
+      },
+      childRT: {
+        required: false,
+        type: Array,
+        default: () => []
+      }
     },
     components: {
       draggable
     },
+    computed:{
+    },
     name: "nested-draggable"
     ,
     methods: {
-        moveTasks(evt){
-            console.log(evt);
+      validateMove(evt) {
+        // console.log('validateMove');
+        // console.log(evt);
+        // console.log(evt);
+        // console.log(this.childRT) 
+        // console.log(evt.relatedContext.component.$parent.childRT);
+        if(evt.relatedContext.component.$parent.childRT.indexOf(evt.draggedContext.element.ty) == -1){
+          // console.log('invalid');
+          evt.willInsertAfter = false;
+          return false;
+        }
+        
+        
+        evt.willInsertAfter = true;
+        return true;
       },
       log(evt) {
-        console.log(evt);
+        // console.log(evt);
       },
       cloneResource(evt) {
-        console.log("cloneResource", evt);
-
+        // console.log("cloneResource", evt);
         return {
           name: evt.name,
           ty: evt.ty,
+          id: uuidv4(),
           tasks: []
         }
-      }
+      },
+      addParentResource(evt) {
+        // console.log("add", evt);
+        // console.log(evt.to); 
+      },
+      RTtoStr(ResourceType) {
+        switch(ResourceType){
+          case RT_CSE:
+            return "CSE";
+          case RT_AE:
+            return "AE";
+          case RT_CNT:
+            return "CNT";
+          case RT_ACP:
+            return "ACP";
+          case RT_GRP:
+            return "GRP";
+          case RT_SUB:
+            return "SUB";
+          case RT_FCNT:
+            return "FCNT";
+          case RT_TS:
+            return "TS";
+        }
+      },
+      getChildRT(parent_ty){
+        // console.log("crt : ", parent_ty, resourceStructure[parent_ty]);
+        return resourceStructure[parent_ty];
+      },
     }
   };
   </script>
