f010c19cf5d78521840dece6a8677ad5bffa2e64
CITRUS-UNSHIU\User <potalcit@gmail.com>
2023-11-08 23:30:31 +0900

    Object parsing

--- oneM2M_Data_Model_Design_Tool/src/json-parser.js
+++ oneM2M_Data_Model_Design_Tool/src/json-parser.js
@@ -13,9 +13,29 @@ function readJSONFile(filePath) {
         const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
         return jsonData;
     } catch (error) {
-        console.error('파일 읽기 오류:', error);
+        console.error('Unable to Read JSON file : ', error);
         return null;
     }
+}
+
+function attribute_check(resource, currentNode ,attribute_list)
+{
+  for (const key of attribute_list) 
+  {
+    if (currentNode.hasOwnProperty(key)) 
+    {
+      resource[key] = currentNode[key];
+    }
+  }
+  console.log(resource);
+  
+  // for key:value check
+  // const entries = Object.entries(resource);
+  // for (const [key, value] of entries) {
+  //   console.log(key + ": " + value);
+  // }
+
+  //return resource;
 }
 
 function make_request_resource(currentNode)
@@ -23,24 +43,37 @@ function make_request_resource(currentNode)
   //ty, rn + resource 형성, http-request에 데이터 전송
   //ty에 따라서 호출해야하는 함수가 다름.. 
 
-  const cnt_resource = ['ty', 'rn', 'lbl', 'acpi', 'at', 'aa', 'cr', 'mni', 'mbs', 'mia'];
+  const cnt_attribute = ['ty', 'rn', 'lbl', 'acpi', 'at', 'aa', 'cr', 'mni', 'mbs', 'mia'];
   // if cr == true, value is NULL
   //
-  const ae_resource = ['ty','rn', 'aei', 'api', 'apn', 'at', 'aa', 'lbl', 'acpi', 'rr', 'poa', 'nl', 'srv'];
-  const acp_resource = ['ty', 'rn', 'ri', 'pi', 'ct', 'lt', 'lbl', 'acpi', 'et', 'st', 'cr', 'pv', 'pvs'];
-  const grp_resource = [];
-  const sub_resource = [];
+  const ae_attribute = ['ty','rn', 'aei', 'api', 'apn', 'at', 'aa', 'lbl', 'acpi', 'rr', 'poa', 'nl', 'srv'];
+  const acp_attribute = ['ty', 'rn', 'ri', 'pi', 'ct', 'lt', 'lbl', 'acpi', 'et', 'st', 'cr', 'pv', 'pvs'];
+  const grp_attribute = [];
+  const sub_attribute = [];
 
   const resource = {};
 
-  console.log(currentNode);
+  // console.log(currentNode);
   console.log(currentNode.ty);
-
-  const entries = Object.entries(currentNode);
-  
-  for (const [key, value] of entries) 
+  if (currentNode.ty == 1)
   {
-  
+    attribute_check(resource, currentNode, acp_attribute);
+  }
+  else if (currentNode.ty == 2)
+  {
+    attribute_check(resource, currentNode, ae_attribute);
+  }
+  else if (currentNode.ty == 3)
+  {
+    attribute_check(resource, currentNode, cnt_attribute);
+  }
+  else if (currentNode.ty == 9)
+  {
+    attribute_check(resource, currentNode, grp_attribute);
+  }
+  else if (currentNode.ty == 23)
+  {
+    attribute_check(resource, currentNode, sub_attribute);
   }
 }
 
@@ -61,7 +94,7 @@ function bfs_json(jsonData) {
         if (currentNode.hasOwnProperty("name") && currentNode.hasOwnProperty("ty")) {
           //console.log(currentNode);
           //console.log(`Name: ${currentNode.name}, Ty: ${currentNode.ty}`);
-          make_request_resource(currentNode);
+          resource = make_request_resource(currentNode);
         }
   
         // 객체의 하위 항목을 큐에 추가
@@ -72,13 +105,13 @@ function bfs_json(jsonData) {
         }
       }
     }
+    return resource;
   }
 
 // JSON 데이터 가져오기
 const jsonObject = readJSONFile(jsonFilePath);
-
 //bfs_json(JSON.stringify(jsonObject, null));
-bfs_json(jsonObject);
+resource = bfs_json(jsonObject);
 
 // if (jsonObject) {
 //     // JSON 데이터를 그대로 출력
