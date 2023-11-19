0c14618025b830114db320d42f7fd8f0b52bb0ea
hh6196 <choi61119666@gmail.com>
2023-11-19 16:34:51 +0900

    Just publish and response check

--- oneM2M_Data_Model_Design_Tool/src/App.vue
+++ oneM2M_Data_Model_Design_Tool/src/App.vue
@@ -70,6 +70,15 @@
         <div class="button" style="background-color: lightblue;" @click="loadFile">
           Load
         </div>
+        <div class="btn button" style="background-color: red;" @click="createConnection">
+          Connect
+        </div>
+        <div class="btn button" style="background-color: blue;" @click="doSubscribe">
+          Sub
+        </div>
+        <div class="btn button" style="background-color: Yellow;" @click="doPublish">
+          Pub
+        </div>
       </div>
     </div>
     <div v-if="attrSetting" class="rightTab">
@@ -110,6 +119,7 @@ import VueDraggableResizable  from "vue-draggable-resizable-vue3";
 import nestedDraggable from "@/components/infra/nested.vue";
 import setAttrs from "@/components/setAttrs.vue";
 import navBar from "@/components/navBar.vue";
+import mqtt from 'mqtt';
 
 const RT_CSE = 5;
 const RT_ACP = 1;
@@ -163,9 +173,60 @@ export default {
       attrSetting : false,
       attrSettingModified: false,
       isDragging: false,
-      selectedElement: {}
-    };
-
+      selectedElement: {},
+      // mqtt connection data field
+      pl : {
+        "fr" : "CSE19",
+        "op" : 1,
+        "pc" : {
+            "m2m:ae" : {
+              "rn": "test",
+              "api": "AE__001",
+              "rr":true
+            }
+        },
+        "rqi" : "m_createAE001",
+        "to" : "TinyIoT",
+        "ty" : 2
+      }
+    ,
+      connection: {
+        protocol: 'ws',
+        host: 'localhost',
+        // ws: 8083; wss: 8084
+        port: 8083,
+        endpoint: '/mqtt',
+        // for more options, please refer to https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
+        clean: true,
+        connectTimeout: 30 * 1000, // ms
+        reconnectPeriod: 4000, // ms
+        clientId:
+          'emqx_vue_' +
+          Math.random()
+            .toString(16)
+            .substring(2, 8),
+        // auth
+        username:'',
+        password:'',
+      },
+      subscription: {
+        topic: '/oneM2M/resp/CSE19/+/#',
+        qos: 0,
+      },
+      publish: {
+        topic: '/oneM2M/req/CSE19/tinyiot/json',
+        qos: 0,
+        payload :'{"fr" : "CSE19", "op" : 1, "pc" : {"m2m:ae" : {"rn": "test2","api": "Rae002","rr":true, "srv": ["2a", "3"]}},"rqi" : "m_createAE003","to" : "TinyIoT", "ty" : 2}'
+      },
+      receiveNews: '',
+      qosList: [0, 1, 2],
+      client: {
+        connected: false,
+      },
+      subscribeSuccess: false,
+      connecting: false,
+      retryTimes: 0,
+    }
   },
   created(){
     const cse = JSON.parse(sessionStorage.getItem("CSE1"));
@@ -181,6 +242,7 @@ export default {
     },
     createResourceTree(){
       console.log("createResourceTree");
+      this.submit();
     },
 
     setAttributes(element){
@@ -246,6 +308,107 @@ export default {
         }
       }
     },
+    submit(){
+      this.createConnection();
+      this.doPublish();
+      this.destroyConnection();
+    },
+    initData() {
+      this.client = {
+        connected: false,
+      }
+      this.retryTimes = 0
+      this.connecting = false
+      this.subscribeSuccess = false
+    },
+    handleOnReConnect() {
+      this.retryTimes += 1
+      if (this.retryTimes > 5) {
+        try {
+          this.client.end()
+          this.initData()
+          this.$message.error('Connection maxReconnectTimes limit, stop retry')
+        } catch (error) {
+          this.$message.error(error.toString())
+        }
+      }
+    },
+    createConnection() {
+      try {
+        this.connecting = true
+        const { protocol, host, port, endpoint, ...options } = this.connection
+        const connectUrl = `${protocol}://${host}:${port}${endpoint}`
+        this.client = mqtt.connect(connectUrl, options)
+        if (this.client.on) {
+          this.client.on('connect', () => {
+            this.connecting = false
+            console.log('Connection succeeded!')
+          })
+          //this.client.on('reconnect', this.handleOnReConnect)
+          this.client.on('error', error => {
+            console.log('Connection failed', error)
+          })
+          this.client.on('message', (topic, message) => {
+            this.receiveNews = this.receiveNews.concat(message)
+            console.log(`Received message ${message} from topic ${topic}`)
+          })
+        }
+      } catch (error) {
+        this.connecting = false
+        console.log('mqtt.connect error', error)
+      }
+    },
+    // subscribe topic
+    // https://github.com/mqttjs/MQTT.js#mqttclientsubscribetopictopic-arraytopic-object-options-callback
+      doSubscribe() {
+      const { topic, qos } = this.subscription
+      this.client.subscribe(topic, { qos }, (error, res) => {
+        if (error) {
+          console.log('Subscribe to topics error', error)
+          return
+        }
+        this.subscribeSuccess = true
+        console.log('Subscribe to topics res', res)
+      })
+    },
+    // unsubscribe topic
+    // https://github.com/mqttjs/MQTT.js#mqttclientunsubscribetopictopic-array-options-callback
+    doUnSubscribe() {
+      const { topic } = this.subscription
+      this.client.unsubscribe(topic, error => {
+        if (error) {
+          console.log('Unsubscribe error', error)
+        }
+      })
+    },
+    // publish message
+    // https://github.com/mqttjs/MQTT.js#mqttclientpublishtopic-message-options-callback
+    doPublish() {
+      const { topic, qos, payload } = this.publish
+      this.client.publish(topic, payload, { qos }, error => {
+        if (error) {
+          console.log('Publish error', error)
+        }
+      })
+      //console.log(JSON.stringify(this.pl))
+    },
+    // disconnect
+    // https://github.com/mqttjs/MQTT.js#mqttclientendforce-options-callback
+    destroyConnection() {
+      if (this.client.connected) {
+        try {
+          this.client.end(false, () => {
+            this.initData()
+            console.log('Successfully disconnected!')
+          })
+        } catch (error) {
+          console.log('Disconnect failed', error.toString())
+        }
+      }
+    },
+    handleProtocolChange(value) {
+      this.connection.port = value === 'wss' ? '8084' : '8083'
+    }
   },
   watch: {
     cse1 : {
